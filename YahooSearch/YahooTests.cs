a3d16081127e51b0a0e87eeff4d859ac84d50d34
sanjeevmajgotra <majgotra@yahoo.com>
2013-11-29 23:37:26 +0530

    Change in class

---
+++ YahooSearch/YahooTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YahooSearch
+{
+    [TestClass]
+    public class YahooTests
+    {
+        [TestMethod]
+        public void Should_Search_Odesk_In_Yahoo()
+        {
+        }
+    }
+}
