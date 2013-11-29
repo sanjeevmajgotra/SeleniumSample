70039a997232a805ad3fccac5731d5d0e30084e5
sanjeevmajgotra <majgotra@yahoo.com>
2013-11-30 00:42:36 +0530

    page load

--- YahooSearch/YahooTests.cs
+++ YahooSearch/YahooTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
 
 namespace YahooSearch
 {
@@ -9,6 +11,8 @@ namespace YahooSearch
         [TestMethod]
         public void Should_Search_Odesk_In_Yahoo()
         {
+            IWebDriver web = new FirefoxDriver ();
+            web.Navigate().GoToUrl("http://www.amazon.com/Best-Sellers-Books-Science-Fiction-Fantasy/zgbs/books/25/ref=zg_bs_nav_b_1_b");
         }
     }
 }
